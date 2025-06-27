d6a7cf9d7dfcaaff3a6fc45c4a63024794ac6521
Nathan Arthur <nathan@nathanarthur.com>
2025-06-27 10:05:04 -0400

    feat(ui): add ContactForm.vue skeleton

---
+++ .vitepress/theme/components/ContactForm.vue
@@ -0,0 +1,15 @@
+<script setup>
+// ContactForm component logic will go here
+</script>
+
+<template>
+  <div class="contact-form">
+    <!-- Contact form template will go here -->
+  </div>
+</template>
+
+<style scoped>
+.contact-form {
+  /* Contact form styles will go here */
+}
+</style>
